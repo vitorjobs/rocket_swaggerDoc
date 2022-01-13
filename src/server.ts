9d0863a328fd484a72be3fdba47194d4fd8a342f
Vitor Jobs <vittorjobs@gmail.com>
2022-01-13 13:52:23 -0300

    Configuraçao da rota do swagger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,30 @@
 import express from 'express'
 import {router} from "./routes"
+import swaggerUI from 'swagger-ui-express'
+import swaggerDoc from "./swagger.json"
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
 
+
+// MEDLEWARE DO SWAGER
+// ROTA PARA A DOCUMENTAÇÃO 
+app.use("/docs", swaggerUI.serve, swaggerUI.setup()) 
+
 app.get('/terms', (request, response) =>{
   return response.json({
     message: "Termos de Serviço"
   })
 })
+
+app.get('/', (request, response) =>{
+  return response.json({
+    message: "Projeto - Documentação com Swagger"
+  })
+})
+
 
 app.use('/v1', router)
 
